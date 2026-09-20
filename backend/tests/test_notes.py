a46a580aac368a56740fa7c1c468a2904ce67de3
armando <231827038+Its-Me573@users.noreply.github.com>
2026-09-19 22:30:42 -0700

    refactor: rewrite test_note_creation function with new create_temp_db function

--- backend/tests/test_notes.py
+++ backend/tests/test_notes.py
@@ -11,34 +11,37 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 
-#each endpoint test will create a temporary .db file, test and then delete that file and revert helper.database_file back to its original value.
+def create_temp_db(database_path):
+    conn = sqlite3.connect(database_path)
+    cur = conn.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Notes(
+        Name TEXT PRIMARY KEY NOT NULL,
+        Content TEXT NOT NULL,
+        "Date Created" TEXT NOT NULL,
+        'Date Modified' TEXT NOT NULL)''')
+
+    return conn, cur
+
 def test_note_creation():
     original_db_file = helper.DATABASE_FILE
     test_db_path = "testFile.db"
 
-    helper.DATABASE_FILE = "testFile.db"
+    helper.DATABASE_FILE = test_db_path
 
-    connection = sqlite3.connect(test_db_path)
-    cursor = connection.cursor()
-
-    #Initialize notes table
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Notes(
-    Name TEXT PRIMARY KEY NOT NULL,
-    Content TEXT NOT NULL,
-    "Date Created" TEXT NOT NULL,
-    'Date Modified' TEXT NOT NULL)''')
-
-    #Temp database has been created correctly from here on
+    conn, cur = create_temp_db(test_db_path)
 
     response = client.post("/notes", json = {
-        "name": "Test Note",
-        "content": "Hello World",
-        "date_created": "1/1/2026 1:00PM",
-        "date_modified": "1/1/2026 1:00PM"
+            "name": "Test Note",
+            "content": "Hello World",
+            "date_created": "1/1/2026 1:00PM",
+            "date_modified": "1/1/2026 1:00PM"
     })
 
     assert response.status_code == 200
     assert response.json()["note_name"] == "Test Note"
 
     helper.DATABASE_FILE = original_db_file
+
+    cur.close
     os.remove(test_db_path)
